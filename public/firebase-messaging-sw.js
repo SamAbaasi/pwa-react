af0367c7fe27afe3b81c9131e7ee1a64eb2fd288
hamed.par <hamed.par@mtnirancell.ir>
2024-08-05 17:25:54 +0330

    change api key

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,16 +6,27 @@ importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
 
 
-// Initialize the Firebase app in the service worker by passing the generated config
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB3Js8U0dzBEWJPaMWnQwkYqUux_mxXOkw",
-  authDomain: "pwa-pushnotification-6b518.firebaseapp.com",
-  projectId: "pwa-pushnotification-6b518",
-  storageBucket: "pwa-pushnotification-6b518.appspot.com",
-  messagingSenderId: "376539431131",
-  appId: "1:376539431131:web:901654eacbe9f2999f8cfe",
-  measurementId: "G-3NRXFNS8H9"
+  apiKey: "AIzaSyCxJ3Zwdp29EBhYG9Z3mTCGHoXQsd35C4Y",
+  authDomain: "fcm-safari-a962a.firebaseapp.com",
+  projectId: "fcm-safari-a962a",
+  storageBucket: "fcm-safari-a962a.appspot.com",
+  messagingSenderId: "367917936686",
+  appId: "1:367917936686:web:3b19900c26e99c02c86fbf",
+  measurementId: "G-40VD45MSTS"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 
 firebase.initializeApp(firebaseConfig);
