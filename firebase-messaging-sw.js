f33290b291ef93b37b07c2ca0a524950abbd2162
hamed.par <hamed.par@mtnirancell.ir>
2024-08-05 17:54:45 +0330

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -8,15 +8,14 @@ importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
-  apiKey: "AIzaSyB3Js8U0dzBEWJPaMWnQwkYqUux_mxXOkw",
-  authDomain: "pwa-pushnotification-6b518.firebaseapp.com",
-  projectId: "pwa-pushnotification-6b518",
-  storageBucket: "pwa-pushnotification-6b518.appspot.com",
-  messagingSenderId: "376539431131",
-  appId: "1:376539431131:web:901654eacbe9f2999f8cfe",
-  measurementId: "G-3NRXFNS8H9"
+  apiKey: "AIzaSyCxJ3Zwdp29EBhYG9Z3mTCGHoXQsd35C4Y",
+  authDomain: "fcm-safari-a962a.firebaseapp.com",
+  projectId: "fcm-safari-a962a",
+  storageBucket: "fcm-safari-a962a.appspot.com",
+  messagingSenderId: "367917936686",
+  appId: "1:367917936686:web:3b19900c26e99c02c86fbf",
+  measurementId: "G-40VD45MSTS"
 };
-
 
 firebase.initializeApp(firebaseConfig);
 
